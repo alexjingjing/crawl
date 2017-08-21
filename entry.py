468944efbb6c50f9cb17f53419ae3264ee150b58
liusiming <abanmiku@hotmail.com>
2017-08-21 16:09:02 +0800

    add countdown

--- entry.py
+++ entry.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 from app.tasks.crawl import crawl_task
 from app.utils.dbutil import *
+from app.utils.randomutil import get_rand_int
 
 if __name__ == '__main__':
     result = get_cities_to_crawl()
@@ -12,4 +13,4 @@ if __name__ == '__main__':
         day_gap_list = eval('[' + day_gap + ']')
         for gap in day_gap_list:
             dep_date = str(datetime.today() + timedelta(days=gap))[0:10]
-            crawl_task.delay(dep_arr.dep_city, dep_arr.arr_city, dep_date)
+            crawl_task.apply_async(dep_arr.dep_city, dep_arr.arr_city, dep_date, count_down=get_rand_int)
