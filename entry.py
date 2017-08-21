5f3f7c947cead629846d659468f24000ffb51162
liusiming <abanmiku@hotmail.com>
2017-08-21 16:20:14 +0800

    fix

--- entry.py
+++ entry.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     result = get_cities_to_crawl()
     for city_info in result:
         dep_arr = city_info[0]
-        day_gap = city_info[1]
+        day_gap = city_info[1].day_gap
         day_gap_list = eval('[' + day_gap + ']')
         for gap in day_gap_list:
             dep_date = str(datetime.today() + timedelta(days=gap))[0:10]
