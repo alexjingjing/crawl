da4f395bd8dbad09b325dd0d1b22b368dc427a3e
liusiming <abanmiku@hotmail.com>
2017-08-24 08:58:19 +0800

    add test

--- entry.py
+++ entry.py
@@ -10,10 +10,11 @@ def crawl_ie_with_cities(dep_city, arr_city):
     city_result = get_ie_cities_to_crawl_with_cities(dep_city, arr_city)
     for city in city_result:
         crawl_task_ie.apply_async((city[0], city[1], city[2], city[3], city[4]),
-                               count_down=get_rand_int())
+                                  count_down=get_rand_int())
 
 
 if __name__ == '__main__':
+    crawl_ie_with_cities('上海', '柬埔寨')
     result = []
     mode = sys.argv[1] if len(sys.argv) > 1 else ''
     if len(mode) > 0 and mode != 'all':
