9c02eb459328c114d740137a25d829dd3cafe5c8
liusiming <abanmiku@hotmail.com>
2017-08-24 08:49:49 +0800

    add method

--- entry.py
+++ entry.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import sys
 
-from app.tasks.crawl import crawl_task
+from app.tasks.crawl import crawl_task, crawl_task_ie
 from app.utils.dbutil import *
 from app.utils.randomutil import get_rand_int
+
+
+def crawl_ie_with_cities(dep_city, arr_city):
+    city_result = get_ie_cities_to_crawl_with_cities(dep_city, arr_city)
+    for city in city_result:
+        crawl_task_ie.apply_async((city[0], city[1], city[2], city[3], city[4]),
+                               count_down=get_rand_int())
+
 
 if __name__ == '__main__':
     result = []
