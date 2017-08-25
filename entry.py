fa7a16633d690ab72a3858a73bcf148a50b510b1
liusiming <abanmiku@hotmail.com>
2017-08-25 10:40:52 +0800

    update

--- entry.py
+++ entry.py
@@ -21,7 +21,7 @@ def crawl_with_dep_city(dep_city):
 
 
 if __name__ == '__main__':
-    crawl_with_dep_city('广州')
+    crawl_with_dep_city('深圳')
     crawl_ie_with_cities('上海', '纽约')
     crawl_ie_with_cities('上海', '柬埔寨')
     get_ie_cities_to_crawl_with_cities('上海', '纽约')
